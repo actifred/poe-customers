b3e71794d0a621e668fb6d6df03ca7e326b447d5
phil <phil@yopmail.com>
2020-05-12 14:33:33 +0200

    apres erreur

--- src/app/ligne-liste-customers/ligne-liste-customers.component.ts
+++ src/app/ligne-liste-customers/ligne-liste-customers.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
-import { EventEmitter } from 'protractor';
+//import { EventEmitter } from 'protractor';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,15 +9,15 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LigneListeCustomersComponent implements OnInit {
   @Input() public currentUser:any;
-  @Output() supprimeEvent = new EventEmitter();
+  //@Output() supprimeEvent = new EventEmitter();
 
   constructor(private httpClient:HttpClient) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public supprime(detailEvenement) {
     console.log('Clic reçu', detailEvenement);
+    console.log("appel du delete pour l'utilisateur d'id " + this.currentUser.id);
     this.httpClient.delete('http://localhost:3000/delete/' + this.currentUser.id);
     //this.supprimeEvent.emit(this.currentUser);
   }
