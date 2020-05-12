3a15a5f536deb9da0dbc4635e6aeacf1924e9cc3
Frederic Mandrea <fred@MacBook-Pro-de-Frederic.local>
2020-05-12 16:14:04 +0200

    Demonstration des takeUntil et take

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserService } from '../services/user.service';
-import { Subscription } from 'rxjs';
-
+import { Subscription, Subject } from 'rxjs';
+import { takeUntil, take } from 'rxjs/operators';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
@@ -11,23 +11,34 @@ export class UserListComponent implements OnInit, OnDestroy {
 
   public users = [];
 
-  private userSub: Subscription;
+
+  private unsubscribe$ = new Subject<any>();
 
   constructor(private userService:UserService) { }
 
   ngOnInit(): void {
-    this.userSub = this.userService.getUsers().subscribe(res => this.users = res);
+    this.userService
+          .getUsers()
+          .pipe(takeUntil(this.unsubscribe$))
+          .subscribe(res => this.users = res);
   }
 
   ngOnDestroy() {
-    this.userSub.unsubscribe();
+    this.unsubscribe$.next();
+    // this.userSub.unsubscribe();
+    // this.deleteSub.unsubscribe();
   }
 
   removeUser(userid) {
-    this.userService.deleteUser(userid).subscribe(() => {
-      console.log('removal of user ' + userid + ' successful');
-      this.users = this.users.filter(user => user.id !== userid);
-    });
+    this.userService.deleteUser(userid)
+      .pipe(take(1))
+      .subscribe(
+        () => {
+          console.log('removal of user ' + userid + ' successful');
+          this.users = this.users.filter(user => user.id !== userid);
+        },
+        err => console.log('Erreur de suppression', err)
+      );
   }
 
 }
